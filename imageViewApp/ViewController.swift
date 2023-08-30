8a22a1323f7967cdb3947444e42742c05a5396bd
Mykaela Wallen <mykaelawallen@gmail.com>
2023-08-30 09:28:08 -0500

    Initial Commit

---
+++ imageViewApp/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  imageViewApp
+//
+//  Created by MYKAELA WALLEN on 8/23/23.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var personOutlet: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func natAction(_ sender: UIButton) {
+        personOutlet.image = UIImage(named: "Nat")
+        
+    }
+    
+    
+    @IBAction func seaverAction(_ sender: UIButton) {
+        personOutlet.image  = UIImage(named: "Seaver")
+    }
+    
+    @IBAction func noelAction(_ sender: UIButton) {
+        personOutlet.image = UIImage(named: "Noel")
+        
+    }
+    
+    
+    
+    
+    
+
+}
+
