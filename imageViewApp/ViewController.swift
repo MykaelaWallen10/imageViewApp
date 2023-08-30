cb65ebddf42205297f698eeaea55d163065dd342
Mykaela Wallen <mykaelawallen@gmail.com>
2023-08-30 09:30:13 -0500

    added a test var

--- imageViewApp/ViewController.swift
+++ imageViewApp/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var personOutlet: UIImageView!
     
+    var test = 7
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
